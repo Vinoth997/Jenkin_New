2a1b509df9a68ffc77688c61a39427626e98d360
vinoth kumar <vinothkumar.c@softsuave.com>
2022-12-20 17:25:09 +0530

    20-12-2022 second

--- src/test/java/practice/sampleTest.java
+++ src/test/java/practice/sampleTest.java
@@ -6,27 +6,46 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class sampleTest {
 	
 
-	public static void main(String[] args) {
-		
-		WebDriver driver;
-		
+	WebDriver driver;
+	
+
+	@BeforeTest
+	public void browserOpen() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testing() {
 		
 		WebElement search = driver.findElement(By.xpath("//input[@class='search-keyword']"));
 		
 		search.sendKeys("C");
-
+		
 	}
+	
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+		
+		
+		
+
+
 
 }
